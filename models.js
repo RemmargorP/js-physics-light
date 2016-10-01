279b69586d9d905e61236b24956645d10f11b75b
Vadim Salavatov <xremmargorpx@gmail.com>
2016-10-01 03:51:57 +0300

    progress

---
+++ models.js
@@ -0,0 +1,57 @@
+function Vector(x=0, y=0, z=0) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+
+  this.neg = function() {
+    return new Vector(-this.x, -this.y, -this.z);
+  };
+
+  this.mul = function(d) {
+    return new Vector(this.x * d, this.y * d, this.z * d);
+  };
+
+  this.sum = function(v) {
+    if (!(v instanceof Vector)) alert('Trying to get vector + not a vector');
+    return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+  };
+
+  this.len = function() {
+    return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+  }
+
+  this.norm = function(len) {
+    if (this.len() == 0) return;
+    return this.mul(len / this.len());
+  };
+
+};
+Vector.prototype = new Vector();
+
+function Segment(a, b) {
+  if (!(a instanceof Vector)) alert('First argument is not a point');
+  if (!(b instanceof Vector)) alert('Second argument is not a point');
+  this.a = a;
+  this.b = b;
+};
+Segment.prototype = new Segment(new Vector(), new Vector());
+
+function Particle(position = new Vector(0, 0, 0), velocity = new Vector(0, 0, 0), acceleration = new Vector(0, 0, 0)) {
+  if (!(position instanceof Vector)) alert('Position is not a vector');
+  if (!(velocity instanceof Vector)) alert('Velocity is not a vector');
+  if (!(acceleration instanceof Vector)) alert('Acceleration is not a vector');
+  this.position = position;
+  this.velocity = velocity;
+  this.acceleration = acceleration;
+
+  this.collide = function() {
+    if (this.position.y <= 0) {
+      this.velocity.y *= -1;
+    }
+  };
+
+  this.update = function(delta_t) {
+    this.velocity = this.velocity.sum(this.acceleration.mul(delta_t));
+    this.position = this.position.sum(this.velocity.mul(delta_t))
+  }
+};
---
+++ simulator.js
@@ -0,0 +1,85 @@
+var simulator = simulator || {};
+var renderer = renderer || {};
+var model = model || {};
+
+renderer.init = function() {
+  renderer.canvas = document.getElementById("canvas");
+  renderer.ctx = renderer.canvas.getContext("2d");
+};
+
+renderer.getPosition = function(v) {
+  return new Vector(10 + v.x, -10 + renderer.canvas.height - v.y, v.z);
+};
+
+renderer.draw = function(model) {
+  with(renderer) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    var pos = renderer.getPosition(new Vector(0, 0));
+    ctx.moveTo(pos.x, pos.y);
+    pos = renderer.getPosition(new Vector(5002, 0));
+    ctx.lineTo(renderer.canvas.width, pos.y);
+    ctx.stroke();
+    ctx.closePath();
+
+    model.particles.forEach(function(item, index, arr) {
+      ctx.beginPath();
+      var pos = renderer.getPosition(item.position);
+      ctx.arc(pos.x, pos.y - 4, 4, 0, Math.PI * 2, false);
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.closePath();
+    });
+  }
+};
+
+model.init = function() {
+  model.constants = {
+    g: new Vector(0, -10, 0)
+  };
+
+  r0 = new Vector(0, 100, 0);
+
+  model.particles = [new Particle(r0, new Vector(), model.constants.g)]
+};
+
+model.update = function(delta_t) {
+  model.particles.forEach(function(item, index, arr) {
+    item.update(delta_t);
+    console.log(item.position.y, item.velocity.y);
+  });
+  model.particles.forEach(function(item, index, arr) {
+    item.collide();
+  });
+}
+
+simulator.init = function() {
+  simulator.renderer = renderer;
+  simulator.renderer.init();
+  simulator.model = model;
+  simulator.model.init();
+};
+
+simulator.draw = function() {
+  simulator.renderer.draw(simulator.model)
+};
+
+simulator.update = function(delta_t) {
+  simulator.model.update(delta_t)
+};
+
+var step = function() {
+  for (i = 0; i < 20; ++i) simulator.update(0.001);
+  simulator.draw();
+}
+
+var sim_id;
+
+function start() {
+  sim_id = setInterval(step, 1);
+}
+
+function stop() {
+  clearInterval(sim_id);
+}
